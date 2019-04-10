135b83997933d4297d926efcc64a14179427f325
ahlemelayeb <49500858+ahlemelayeb@users.noreply.github.com>
2019-04-11 01:19:40 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,23 +4,72 @@
 #include <SDL/SDL_mixer.h>
 #include <time.h>
 #include <unistd.h>
-#include "objet.h"
+#include "ennemi.h"
+#include "map.h"
+#include "vie.h"
+#include "perso.h"
+#include "collision.h"
+#include "scrolling.h"
+#include "input.h"
 
-void main(){
 
-objet objet ;
-int continuer=1 ;
-SDL_Surface *ecran ;
+int main(int argc, char *argv[])
+{
+perso perso ;
+vie vie;
+map map ;
+
+ennemis ennemi ;
+SDL_Rect pos;
+int q,w=10;
+
+pos.x=0;
+pos.y=0;
+
+SDL_Rect camera = { 0, 0, 1000, 600 };
+
+camera.x=0;
+camera.y=0;
+SDL_Surface *ecran = NULL;
+
+srand(time(NULL)) ;
+
+
+int d,x=0,y=1;
+int continuer = 1;
+
+SDL_Event event;
+
 ecran = SDL_SetVideoMode(1000, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE);
 
-initialiseobjet(&objet);
+initialiserperso(&perso);
+initialiserfond(&map);
+initialiserennemi(&ennemi);
+initialiservie(&vie) ;
+
+SDL_EnableKeyRepeat(300,10);
+SDL_ShowCursor(SDL_DISABLE);
+SDL_LockSurface;
 while (continuer)
 {
-afficherobjet(objet,&ecran);
+d=direction(event,&continuer);
+
+x = mouv(d,x);
+y=splitennemi(y);
+
+camera = scrolling(d,camera,perso);
+
+perso=mouvement(perso,camera,d);
+ennemi=mouvennemi(ennemi,d,camera,&q,&w,&y);
+collisionennemi(&perso,&ennemi,&camera,&vie);
+
+
+afficherfond(map,camera,ecran);
+afficherperso(perso,ecran,x);
+afficherennemi(ennemi,ecran,y);
+affichervie(&vie,ecran);
 SDL_Flip(ecran);
 SDL_Delay(80);
 }
-
-
-
+return 0 ;
 }
